97c205aea2ba591d45600c0992bf0f2229c2d6e0
오서진 <seojin2414@gmail.com>
2025-07-07 22:46:08 +0900

    Initial Commit

---
+++ DodinDiary/DodinDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DodinDiaryApp.swift
+//  DodinDiary
+//
+//  Created by Oh Seojin on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DodinDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
